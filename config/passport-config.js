b6dc60a74dd111305ce759f39a4d77e1faa6245c
unknown <alvintonae@gmail.com>
2026-06-05 20:04:59 +0300

    Essential AI Corrections

--- config/passport-config.js
+++ config/passport-config.js
@@ -8,7 +8,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.LOCALHOST}/auth/google/redirect`
+    callbackURL: `${process.env.OMAH_DOMAIN}/auth/google/redirect`
 
 }, (accessToken, refreshToken, profile, done ) => {
 
