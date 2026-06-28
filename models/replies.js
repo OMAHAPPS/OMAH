9e8bb4ef56d306b58962af5acdf59bcc408b34c5
unknown <alvintonae@gmail.com>
2026-06-28 20:51:33 +0300

    Major reply webs initiated

--- models/replies.js
+++ models/replies.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema
 const replySchema = new Schema({
 
      userId: { type: String, required: true },
-     postId: { type: String, required: true },
+     postId: { type: String, required: true },          // postId will either be main post ID or a Reply Id
+     replyType: { type: String, required: true },       // parent || child if it is sub-reply
      replystring: { type: String, default: 'none', required: false },
      images: { type: Array, default: [], required: false },
      videoUrl: { type: String, default: 'none', required: false },
