1e5f7c5c60c346b679480e75cc1b70f2a2e09f00
unknown <alvintonae@gmail.com>
2026-07-01 13:09:38 +0300

    Badge trial

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,7 @@ self.addEventListener('push', (event) => {
     const options = {
       body: payload.body,
       icon: payload.icon || 'https://pub-4d616d04d586465385a8e29f389675d7.r2.dev/feed-images/1781109238815-Xikika_ICON.jpeg',
-      badge: 'https://pub-4d616d04d586465385a8e29f389675d7.r2.dev/feed-images/1781109238815-Xikika_ICON.jpeg',
+      badge: 'XIKIKA-KE.png',
       data: payload.data, // Holds context meta fields like roomId
       vibrate: [200],
       actions: [
